52d835c0ed43d9d46fdacc6a416da0ca29cd3c07
martina <martina.bevilacqua1992@gmail.com>
2020-12-15 17:24:56 +0100

    add demo and API tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SequenceViewer} from 'sequence-viewer-typescript/dist';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sequence-viewer-documentation';
+
+  public s0;
+  public seqs;
+
+  public openPage(pageName) {
+    let i;
+    let tabcontent;
+    let tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablink');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = '';
+    }
+    document.getElementById(pageName).style.display = 'block';
+  }
+
+  public openPage2(pageName) {
+    let i;
+    let tabcontent;
+    let tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent2');
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablink2');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = '';
+    }
+    document.getElementById(pageName).style.display = 'block';
+  }
+
+  ngOnInit(): void {
+    document.getElementById('defaultOpen').click();
+    document.getElementById('defaultOpen2').click();
+
+    this.s0 = new SequenceViewer('sqv0');
+    this.seqs = [
+      {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 11, label: 'pip5human', startIndex: 30},
+      {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 11, label: 'pip5human', startIndex: 30}];
+    this.s0.draw(this.seqs);
+  }
+
+
+
+
 }
