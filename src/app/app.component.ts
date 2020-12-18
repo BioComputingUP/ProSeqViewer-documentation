58dbd7000561964798f0f66cb23aae1219cae3cc
martina <martina.bevilacqua1992@gmail.com>
2020-12-18 10:03:32 +0100

    add icons example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,75 +9,56 @@ import {SequenceViewer} from 'sequence-viewer-typescript/dist';
 export class AppComponent implements OnInit {
   title = 'sequence-viewer-documentation';
 
-  public openPage(pageName) {
+  public openPage(pageName, n) {
     let i;
     let tabcontent;
     let tablinks;
-    tabcontent = document.getElementsByClassName('tabcontent');
+    tabcontent = document.getElementsByClassName('tabcontent' + n);
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = 'none';
     }
-    tablinks = document.getElementsByClassName('tablink');
+    tablinks = document.getElementsByClassName('tablink' + n);
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = '';
     }
-    document.getElementById(pageName).style.display = 'block';
-  }
-
-  public openPage1(pageName) {
-    let i;
-    let tabcontent;
-    let tablinks;
-    tabcontent = document.getElementsByClassName('tabcontent1');
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = 'none';
-    }
-    tablinks = document.getElementsByClassName('tablinks1');
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = '';
-    }
-    document.getElementById(pageName).style.display = 'block';
-  }
-
-  public openPage2(pageName) {
-    let i;
-    let tabcontent;
-    let tablinks;
-    tabcontent = document.getElementsByClassName('tabcontent2');
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = 'none';
-    }
-    tablinks = document.getElementsByClassName('tablinks2');
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = '';
-    }
-    document.getElementById(pageName).style.display = 'block';
+    console.log(pageName)
+    document.getElementById(pageName + n).style.display = 'block';
   }
 
   ngOnInit(): void {
     document.getElementById('defaultOpen').click();
     document.getElementById('defaultOpen1').click();
     document.getElementById('defaultOpen2').click();
+    document.getElementById('defaultOpen3').click();
 
-
-    const s0 = new SequenceViewer('sqv0');
+    // SIMPLE EXAMPLE
+    const s1 = new SequenceViewer('sqv1');
     const seqs = [
       {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 1, label: 'Human'},
       {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 2, label: 'Mice'}];
-    s0.draw(seqs);
+    s1.draw(seqs);
 
-
-    const s1 = new SequenceViewer('sqv1');
-    const seqs1 = [
-      {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 1, label: 'Human'},
-      {sequence: 'DFRLELERELYRLELEAA------------AAARAAAAAELYRLELER---F-------RR', id: 2, label: 'Mice'}];
-    const reg1 = [{sequenceId: 1, start: 10, end: 15, backgroundImage: 'linear-gradient(red, yellow)'},
-      {sequenceId: 1, start: 1, end: 6, backgroundColor: '(180, 180, 180)', color: '#F7F06D'},
-      {sequenceId: 2, start: 10, end: 15},
-      {sequenceId: 2, start: 18, end: 25, backgroundColor: '(210, 100, 180)'},
-      {sequenceId: 2, start: 30, end: 35}
+    // REGIONS EXAMPLE
+    const s2 = new SequenceViewer('sqv2');
+    const reg1 = [{sequenceId: 1, start: 1, end: 10, backgroundImage: 'linear-gradient(red, yellow)'},
+      {sequenceId: 1, start: 31, end: 40, backgroundColor: '#FF715B', color: '#F7F06D'},
+      {sequenceId: 1, start: 41, end: 50, backgroundColor: '(255, 173, 10)'}
     ];
-    s1.draw(seqs1, reg1);
+    s2.draw(seqs, reg1);
+
+
+    // icons EXAMPLE
+    const s3 = new SequenceViewer('sqv3');
+    const ico2 = [{sequenceId: 1, start: 1, end: 10 },
+      {sequenceId: 1, start: 11, end: 21, icon: 'turn'},
+      {sequenceId: 1, start: 21, end: 30, icon: 'helix'},
+      {sequenceId: 1, start: 31, end: 40, icon: 'lollipop'},
+      {sequenceId: 1, start: 41, end: 41, icon: 'arrowLeft'},
+      {sequenceId: 1, start: 42, end: 50, icon: 'strand'},
+      {sequenceId: 1, start: 48, end: 50, icon: 'noSecondary'}
+    ];
+    s3.draw(seqs, ico2);
+
   }
 
 
