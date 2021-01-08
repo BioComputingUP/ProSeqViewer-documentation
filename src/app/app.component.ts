2685459407635c5b65eafa8e9a54a818c05ffb1f
martina <martina.bevilacqua1992@gmail.com>
2021-01-08 16:27:59 +0100

    add consensus palettes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,14 +38,7 @@ export class AppComponent implements OnInit {
       {sequence: 'LALQAKQYGDFDRYVLALSWQTGFCQSQHD-RNRNERDECRLQTETTNKADFLTVHGLWPGLPKSVAARGVDERRWMRFGCATRPIPNLPEARASRMCSSPETGLSLETAAKLSEVMPGAGG-----RSCLERYEYAKHGACFG----F-DPDAYFGTMVRLNQ--EIKESEAGKF-LADNYGK-TVSRRDFDAAFAK-SWGKENVKAVKLTCQGNP-----AYLTEIQISIKADAINAPLSANSFLPQPHP-----------------------GNCGKTFVI-D-K-----AGY---', id: 6, label: '2pqxA'},
       {sequence: 'DKR-LRDNHEWKKLIMVQHWPETVCEKIQN-DCR------------D-PPDYWTIHGLWPDKSEG---------------CNRSWPFNLEEI----------KDLLPEMRAYWPDVIHSFPN-----RSRFWKHEWEKHGTCAAQVDALNSQKKYFGRSLELYR--ELDLNSVLLKLGIKPSIN-YYQVADFKDALAR-VYGVI----PKIQCLPPSQDEEVQTIGQIELCLTK-------QDQQLQNCTEP-GEQPSPKQEVWLANGAAESRGLRVCEDGPVFYPPPKKTKHHHHHHH', id: 7, label: '3t0oA'}
     ];
-    const ico3 = [
-      {sequenceId: 1, start: 11, end: 20, icon: 'turn'},
-      {sequenceId: 1, start: 21, end: 30, icon: 'helix'},
-      {sequenceId: 1, start: 31, end: 40, icon: 'lollipop'},
-      {sequenceId: 1, start: 41, end: 41, icon: 'arrowLeft'},
-      {sequenceId: 1, start: 42, end: 50, icon: 'strand'},
-      {sequenceId: 1, start: 48, end: 50, icon: 'noSecondary'}
-    ];
+
 
     const opt = { topIndexes: true, colorScheme: 'blosum62', consensusType: 'identity'};
     const reg = [
@@ -56,7 +49,7 @@ export class AppComponent implements OnInit {
     // GENERAL EXAMPLE: multisequence
     const seqSingle = new SequenceViewer('sqvSingle');
     const seq = [
-      {sequence: '---------DFEYLQLVLTWPASFCYANH---CE------------RIAPNNFTIHGLWPDNVKT-RLHN----------CKPKPTYSY-FT----------GKMLNDLDKHWMQLKFEQDY--GRTEQPSWKYQYIKHGSCCQKR--Y-NQNTYFGLALRLKD--KFDLLRTLQTHRIIP-GS-SYTFQDIFDAIKT-VSQEN----PDIKCAEVTKG--TPELYEIGICFTP------ -NADSMFRCPQS-D----------------------TCDKTAKV-L-F-----RR----', id: 1, label: '1iooA'}
+      {sequence: 'MKTTRRSFLKLVGVSVVGLSLGQLGFDLEDAQAYAVKLKIEGAKEVGTVCPFCSVCCQVIAYVRNGKLVSTEGDPDFPVNEGALCAKGAALFSMYTNPHRLTKPLYRAPHSDKWVEKDWDWTLNQIARRVKDARDKDMILKNDKGQTVNRLESIFMMGTSHASNEECAVIHQAMRGLGVVHMDHQARVUHSPTVAALAESFGRGAMTNHWIDIKNTDAVLIIGSNAAEHHPVAFKWIMRARDNGAVLMHVDPKFSRTSARCDFHVPLRSGTDIAFLGGMVNHIIAKDLYFKDYVANYTNAAFVVGKDYAFEDGIFSGYDPKTRTYDRSKWEFEKGPDGGPVMDPTLKNERCVFNLMKKHYERYTLKNVSDVTGVSEENLLRVYDAFCATGRPDKAGTILYALGWTQHTVGVQNIRTSTLIQLLLGNIGVAGGGINALRGEPNVQGSTDHALLYHILPGYNAMPVAQWQTLADYNKANTPVTTLKNSANWWSNRPKYVASLLKGWFGDAATPENDFCYEYLPKLEKGEDYSYMYVMDRMYHGKLKGGFIFGVNPMNSFPNTNKMRAALDKLDWLVCSELHNSETTDNWKRPGVDPKACKTEVFLLPSAHRVEKAGTISNSGRWLQWFDKAVEPGQARNFADIFVPLVNKIRALYKAEGGTLPDPVLKLHWTDKFDPEEWTRRINGFFWADTKVGDKEYKRGQLVPAFGQLKDDGSTSSLNWLYTGSYTEEDGNKSKRRDARQTPMQANIGLFPNWSWCWPVNRRILYNRASVDVNGKPWNPKKAVIEWDGAKWVGDVPDGPWPPMADKEKGKLPFIMNKDGFAQFYGTGRMDGPFPEHYEPAETPLDSHPFSKQLSSPVYKFHTSDMDQIAKAADPKYPIVLTTYSLTEHWCGGGETRNVPNLLETEPQLYIEMSPELAEEKGIKNGDGVIVESIRGRAEAIAMVTVRIRPFTVMGKTVHLVGMPFAYGWTTPKCGDSTNRLTVGAYDPNTTIPESKACLVNVRKADKLTEIA', id: 1, label: 'Q727P3'}
     ];
 
     seqSingle.draw(seq);
@@ -75,32 +68,53 @@ export class AppComponent implements OnInit {
     // REGIONS EXAMPLE
     const s2 = new SequenceViewer('sqv2');
     const seqs = [
-      {sequence: 'MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHG', id: 1, label: 'sp|P69905|HBA_HUMAN'},
+      {sequence: 'MVLSPADKTNVKAAWGKVGAHGGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHG', id: 1, label: 'sp|P69905|HBA_HUMAN'},
       {sequence: 'MVLSGEDKSNIKAAWGKIGGHGAEYGAEALERMFASFPTTKTYFPHFDVSHGSAQVKGHG', id: 2, label: 'sp|P01942|HBA_MOUSE'},
-      {sequence: 'MSLTRTERTIILSLWSKISTQADVIGTETLERLFSCYPQAKTYFPHFDLHSGSAQLRAHG', id: 3, label: 'sp|P13786|HBAZ_CAPHI'}
+      {sequence: 'MSLTRTERTIILSLWSKISTQADVIGTETLERLFSCYPQAKTYFPHFDVSSGSAQLRAHG', id: 3, label: 'sp|P13786|HBAZ_CAPHI'}
     ];
     const reg1 = [
       {sequenceId: 2, start: 1, end: 10, backgroundColor: '#D72638', color: '#FFFFFF'},
       {sequenceId: 2, start: 11, end: 20, backgroundColor: '#FF570A', color: '#FFFFFF'},
+      {sequenceId: 1, start: 21, end: 30, backgroundColor: '#FFBC42', color: '#FFFFFF'},
       {sequenceId: 2, start: 21, end: 30, backgroundColor: '#FFBC42', color: '#FFFFFF'},
       {sequenceId: 2, start: 31, end: 40, backgroundColor: '#44885B', color: '#FFFFFF'},
       {sequenceId: 2, start: 41, end: 50, backgroundColor: '#393C93', color: '#FFFFFF'},
+      {sequenceId: 3, start: 41, end: 50, backgroundColor: '#393C93', color: '#FFFFFF'},
       {sequenceId: 2, start: 51, end: 60, backgroundColor: '#7E1173', color: '#FFFFFF'},
     ];
     s2.draw(seqs, reg1);
 
     // icons EXAMPLE
     const s3 = new SequenceViewer('sqv3');
-    s3.draw(seqs, ico3);
+    const ico3 = [
+      {sequenceId: 1, start: 1, end: 5, icon: 'turn'},
+      {sequenceId: 1, start: 6, end: 15, icon: 'noSecondary'},
+      {sequenceId: 1, start: 15, end: 19, icon: 'strand'},
+      {sequenceId: 1, start: 20, end: 20, icon: 'arrowRight'},
+      {sequenceId: 1, start: 21, end: 25, icon: 'helix'},
+      {sequenceId: 1, start: 26, end: 30, icon: 'noSecondary'},
+      {sequenceId: 1, start: 31, end: 35, icon: 'strand'},
+      {sequenceId: 1, start: 36, end: 36, icon: 'arrowRight'},
+      {sequenceId: 1, start: 37, end: 46, icon: 'helix'},
+      {sequenceId: 1, start: 47, end: 47, icon: 'noSecondary'},
+      {sequenceId: 1, start: 47, end: 49, icon: 'lollipop', display: 'center'},
+      {sequenceId: 1, start: 49, end: 54, icon: 'noSecondary'},
+      {sequenceId: 1, start: 55, end: 60, icon: 'turn'},
+    ];
+    const opt3 = {chunkSize: 60}; // sequence length
+    s3.draw(seqs, ico3, opt3);
 
     // patterns EXAMPLE
     const s4 = new SequenceViewer('sqv4');
     const pat4 = [
-      {sequenceId: 1, pattern: /MVL/g, backgroundColor: '#D72638'},
       {sequenceId: 1, pattern: /GA/g, backgroundColor: '#D72638'},
-      {sequenceId: 2, pattern: /LE/g, backgroundColor: '#FF570A'},
+      {sequenceId: 2, pattern: /GA/g, backgroundColor: '#D72638'},
+      {sequenceId: 1, pattern: /TT/g, backgroundColor: '#FF570A'},
       {sequenceId: 2, pattern: /TT/g, backgroundColor: '#FF570A'},
-      {sequenceId: 3, pattern: /QA/g, backgroundColor: '#FFBC42'}
+      {sequenceId: 2, pattern: /VS/g, backgroundColor: '#FFBC42'},
+      {sequenceId: 3, pattern: /VS/g, backgroundColor: '#FFBC42'},
+      {sequenceId: 2, pattern: /VS/g, backgroundColor: '#FFBC42'},
+      {sequenceId: 3, pattern: /VS/g, backgroundColor: '#FFBC42'}
     ];
     s4.draw(seqs, pat4);
 
@@ -117,7 +131,7 @@ export class AppComponent implements OnInit {
     // indexes EXAMPLE
     const s5 = new SequenceViewer('sqv5');
     const seqs5 = [
-      {sequence: 'MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHG', id: 1, label: 'sp|P69905|HBA_HUMAN', startIndex: 2}
+      {sequence: 'MVLSPADKTNVKAA-----EYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHG', id: 1, label: 'sp|P69905|HBA_HUMAN', startIndex: 2}
     ];
     const opt5 = { lateralIndexesGap: true};
     s5.draw(seqs5, opt5);
@@ -133,6 +147,15 @@ export class AppComponent implements OnInit {
     ];
     const opt7 = { consensusType: 'identity', consensusThreshold: 80};
     s7.draw(seqs7, opt7);
+
+    const sPhysical = new SequenceViewer('sqvPhysical');
+    const seqsPhysical = [
+      {sequence: 'MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHG', id: 1, label: 'sp|P69905|HBA_HUMAN'},
+      {sequence: 'MVLSGEDKSNIKAAWGKIGGHGAEYGAEALERMFASFPTTKTYFPHFDVSHGSAQVKGHG', id: 2, label: 'sp|P01942|HBA_MOUSE'},
+      {sequence: 'MSLTRTERTIILSLWSKISTQADVIGTETLERLFSCYPQAKTYFPHFDLHSGSAQLRAHG', id: 3, label: 'sp|P13786|HBAZ_CAPHI'}
+    ];
+    const optPhysical = { consensusType: 'physical'};
+    sPhysical.draw(seqsPhysical, optPhysical);
 
     // oneLineSetting EXAMPLE
     const s8 = new SequenceViewer('sqv8');
