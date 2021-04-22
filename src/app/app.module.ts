de6aed2799255c9b9d311f83c65d9dc34aaeb8cd
martina <martina.bevilacqua1992@gmail.com>
2021-04-22 15:26:24 +0200

    update package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-
+import {ProSeqViewer} from 'proseqviewer/dist';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, TabsModule.forRoot()],
