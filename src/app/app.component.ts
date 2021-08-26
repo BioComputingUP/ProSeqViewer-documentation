0b018fa8412585cc7c6d6e4d0546f8806c9060fa
mb925 <martina.bevilacqua5@libero.it>
2021-08-26 16:31:39 +0200

    loading section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -226,8 +226,9 @@ export class AppComponent implements OnInit {
           {sequenceId: 1, pattern: /GV/g, backgroundColor: '#FFBC42'},
       ];
 
+      const txt = /AA/g ;
       this.patterns = {patterns: [
-          {sequenceId: 1, pattern: /AA/g, backgroundColor: '#D72638'}
+          {sequenceId: 1, pattern: txt, backgroundColor: '#D72638'}
       ]};
 
       s4.draw({patterns: pat4, sequences: seqs});
@@ -384,12 +385,66 @@ export class AppComponent implements OnInit {
       // selections EXAMPLE
       const s9 = new ProSeqViewer('sqv9');
       const seqs9 = [
-          {sequence: 'MDIAIHHPWIRRPFFPFHSPSRLFDQFFGEHLLESDLFPTSTSLSPFYLRPPSFLRAPSW', id: 1, label: 'CRAB_HUMAN'},
-          {sequence: 'MDITIHNPLIRRPLFSWLAPSRIFDQIFGEHLQESELLPASPSLSPFLMRSPI-FRMPSW', id: 2, label: 'CRAB_ANAPL'},
-          {sequence: 'MDITIHNPLVRRPLFSWLTPSRIFDQIFGEHLQESELLPTSPSLSPFLMRSPF-FRMPSW', id: 3, label: 'CRAB_CHICK'}
+          {
+              sequence: 'GTREVPADAYYGVHTLRAIENFYISNNKISDIPEFVRGMVMVKKAAAMANKELQTIPKSV' +
+                  'ANAIIAACDEVLNNGKCMDQFPVDVYQGGAGTSVNMNTNEVLANIGLELMGHQKGEYQYLNPNDHVNKCQSTNDAYPTGFRIAV',
+              id: 1, label: 'ASPA_ECOLI/13-157'
+          },
+          {
+              sequence: 'GEKQIEADVYYGIQTLRASENFPITGYKIHEE..MINALAIVKKAAALANMDVKRLYEGIGQAIVQAADEILE.GKWHDQFIVDPIQGGA' +
+                  'GTSMNMNANEVIGNRALEIMGHKKGDYIHLSPNTHVNMSQSTNDVFPTAIHIST',
+              id: 2, label: 'ASPA_BACSU/16-157'
+          },
+          {
+              sequence: 'MKYTDTAPKLFMNTGTKFPRRIIWS.............MGVLKKSCAKVNADLGLLDKKIADSIIKASDDLID.GKLDDKIVLDVFQTGSGTGLNMNVNEV' +
+                  'IAEVASSYSN......LKVHPNDHVNFGQSSNDTVPTAIRIAA',
+              id: 3, label: 'FUMC_SACS2/1-126'
+          },
+          {
+              sequence: 'GRFTQAADQRFKQFNDSLRFDYRLAEQDIV.......GSVAWSKALVTVGVLT....AEEQAQLEEALNVLLEDVRARPQQILESDAEDIHSWVEGKL' +
+                  'IDKVG.................QLGKKLHTGRSRNDQVATDLKLWC',
+              id: 4, label: 'ARLY_ECOLI/6-122'
+          },
+          {
+              sequence: 'GRFVGAVDPIMEKFNASIAYDRHLWEVDVQ.......GSKAYSRGLEKAGLLT....KAE' +
+                  'MDQILHGLDKVAEEWAQG.TFKLNSNDEDIHTANERRLKELIG.................ATAGKLHTGRSRNDQVVTD' +
+                  'LRLWM',
+              id: 5, label: 'ARLY_HUMAN/11-127'
+          },
+          {
+              sequence: 'GGRFSGATDPLMAEFNKSIYSGKEMCEEDVI.......GSMAYAKALCQKNVIS....EEELNSILKGLEQIQREWNSG.QFVLEPSDEDVHTANERRLTEI' +
+                  'IG.................DVAGKLHTGRSRNDQVTTDLRLW',
+              id: 6, label: 'ARLY_SCHPO/12-128'
+          },
+          {
+              sequence: 'GRFTGATDPLMDLYNASLPYDKVMYDADLT.......GTKVYTQGLNKLGLIT....TEELHLIHQGLEQIRQEWHDN.KFIIKAGDEDIHTANERRLG' +
+                  'EIIG................KNISGKVHTGRSRNDQVATDMRIFV',
+              id: 7, label: 'Q59R31_CANAL/14-130'
+          },
+          {
+              sequence: 'GRFTGKTDPLMEKFNESLPFDKRLWAEDIK.......GSQAYAKALAKAGILT....HVE' +
+                  'AASIVDGLSKVAEEWQSG.VFVVKPGDEDIHTANERRLTELIG.................' +
+                  'AVGGKLHTGRSRNDQVATDYRLWL',
+              id: 8, label: 'A0A125YZR4_VOLCA/23-139'
+          },
+          {
+              sequence: 'ERYS..RPEMSAIWTDENRFQAWLE............VEILACEAWAELGVIP.......' +
+                  '.KEDVKVMRENAS.FDINRILEIEKDTRHDVVAFTRAVSESLG.................' +
+                  'EERKWVHYGLTSTDVVDTALSYLL',
+              id: 9, label: 'PUR8_BACSU/3-108'
+          },
+          {
+              sequence: 'GRYGDKVSALRGIFSEYGLLKFRVQVEVR........WLQKLAAHAAIKEVPA......F' +
+                  'AADAIGYLDAIVASFSEEDAARIKTIERTTNHDVKA.VEYFLK....EKVAE...IPELH' +
+                  'AVSEFIHFACTSEDINNLSHALML',
+              id: 10, label: 'PUR8_ECOLI/14-137'
+          }
       ];
-      s9.draw({sequences: seqs9});
+      s9.draw({sequences: seqs9, options: {selection: 'areaselection'}});
 
+      // selections EXAMPLE
+      const scolumns = new ProSeqViewer('sqvcolumn');
+      scolumns.draw({sequences: seqs9, options: {selection: 'columnselection'}});
 
       window.addEventListener('onRegionSelected', (e) => {
           console.log('character selected', e['detail'].char);
